3ac8efe4eb47a80f3a2a335ae80b619c6f856bd1
manfmexistence <ajju40959@gmail.com>
2025-05-29 00:18:43 +0600

    fix: update span styles to use React.CSSProperties for better type safety

--- www/src/app/page.tsx
+++ www/src/app/page.tsx
@@ -197,34 +197,34 @@ export default function Home() {
       </div>
       {/* <SignUp />
       <SignIn /> */}
-          <div className="hello">
-        <span className="start" style="--i:1;"></span>
-        <span style="--i:2;"></span>
-        <span style="--i:3;"></span>
-        <span style="--i:4;"></span>
-        <span style="--i:5;"></span>
-        <span style="--i:6;"></span>
-        <span style="--i:7;"></span>
-        <span style="--i:8;"></span>
-        <span style="--i:9;"></span>
-        <span style="--i:10;"></span>
-        <span style="--i:11;"></span>
-        <span style="--i:12;"></span>
-        <span style="--i:13;"></span>
-        <span style="--i:14;"></span>
-        <span style="--i:15;"></span>
-        <span style="--i:16;"></span>
-        <span style="--i:17;"></span>
-        <span style="--i:18;"></span>
-        <span style="--i:19;"></span>
-        <span className="end" style="--i:20;"></span>
-    </div>
+      <div className="hello">
+        <span className="start" style={{ "--i": 1 } as React.CSSProperties}></span>
+        <span style={{ "--i": 2 } as React.CSSProperties}></span>
+        <span style={{ "--i": 3 } as React.CSSProperties}></span>
+        <span style={{ "--i": 4 } as React.CSSProperties}></span>
+        <span style={{ "--i": 5 } as React.CSSProperties}></span>
+        <span style={{ "--i": 6 } as React.CSSProperties}></span>
+        <span style={{ "--i": 7 } as React.CSSProperties}></span>
+        <span style={{ "--i": 8 } as React.CSSProperties}></span>
+        <span style={{ "--i": 9 } as React.CSSProperties}></span>
+        <span style={{ "--i": 10 } as React.CSSProperties}></span>
+        <span style={{ "--i": 11 } as React.CSSProperties}></span>
+        <span style={{ "--i": 12 } as React.CSSProperties}></span>
+        <span style={{ "--i": 13 } as React.CSSProperties}></span>
+        <span style={{ "--i": 14 } as React.CSSProperties}></span>
+        <span style={{ "--i": 15 } as React.CSSProperties}></span>
+        <span style={{ "--i": 16 } as React.CSSProperties}></span>
+        <span style={{ "--i": 17 } as React.CSSProperties}></span>
+        <span style={{ "--i": 18 } as React.CSSProperties}></span>
+        <span style={{ "--i": 19 } as React.CSSProperties}></span>
+        <span className="end" style={{ "--i": 20 } as React.CSSProperties}></span>
+      </div>
     </div>
   )
 }
 
-      {/* <Friday orbSize={100} shapeSize={90} />  */}
-      {/* <h1 className="bold w-full text-center font-sans text-3xl">
+{/* <Friday orbSize={100} shapeSize={90} />  */ }
+{/* <h1 className="bold w-full text-center font-sans text-3xl">
         {greeting && `${greeting}, Sumon.`}
       </h1>
       <AiInput
@@ -232,7 +232,7 @@ export default function Home() {
         onInputChange={setCurrentInput}
         onSubmit={() => setHasSubmitted(true)}
       /> */}
-      {/* <div className="animate-content-height w-full transition-all duration-500 ease-in-out">
+{/* <div className="animate-content-height w-full transition-all duration-500 ease-in-out">
         {currentInput.trim() ? (
           <SearchSuggestions
             inputValue={currentInput}
