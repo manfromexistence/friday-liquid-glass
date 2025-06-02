a3ffa00208bfcbd65dade96fe4c1cd6fee917d86
manfromexistence01 Emon <ajju40959@gmail.com>
2025-06-02 12:46:46 +0000

    feat: all translations are done

--- www/src/locales/main.py
+++ www/src/locales/main.py
@@ -11,7 +11,8 @@ language_codes = [
     # "ee","tl","fi","fr","fy","gl","ka","de","el","gn",
     # "gu","ht","ha","haw","iw","hi","hmn","hu",
     # "is","ig","ilo","id","ga","it","ja",
-    "jw","kn","kk","km","rw","gom","ko","kri","ku","ckb","ky","lo","la","lv","ln","lt",
+    "jw","kn","kk","km","rw","gom","ko","kri","ku","ckb",
+    "ky","lo","la","lv","ln","lt",
     "lg","lb","mk","mai","mg","ms","ml","mt","mi","mr","mni-Mtei","lus","mn","my","ne","no","or","om","ps","fa","pl","pt","pa",
     "qu","ro","ru","sm","sa","gd","nso","sr","st","sn","sd","si","sk","sl","so","es","su","sw","sv","tg","ta","tt","te","th",
     "ti","ts","tr","tk","ak","uk","ur","ug","uz","vi","cy","xh","yi","yo","zu"
