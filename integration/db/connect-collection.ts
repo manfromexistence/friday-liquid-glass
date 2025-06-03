dda4940703e256c6fcc4e3ad7d6a87198ffce27f
manfromexistence01 Emon <ajju40959@gmail.com>
2025-06-03 14:55:29 +0000

    feat: now I get datastax astra

--- integration/db/connect-collection.ts
+++ integration/db/connect-collection.ts
@@ -3,8 +3,11 @@ import type { VectorizeDoc } from "@datastax/astra-db-ts";
 
 
 export function connectToDatabase(): Db {
-  const endpoint = "https://343d000d-f560-48cd-ad81-3e6197ab4a41-us-east-2.apps.astra.datastax.com";
-  const token = "AstraCS:iumnAmyrXRffWPpgUotrTZia:5983fefa5056bb8b0347e9acd70230608af281a81026da70261b83ae01fa564e";
+  const endpoint = "https://e8fbcc91-ca2e-4c64-8fc0-d3a019005ff4-us-east-2.apps.astra.datastax.com";
+  const token = "AstraCS:zKXXpxTroUBzGLniPbIrOaUw:4744720afb5ccac6c5b4b63c5e5292c07664b595ea9b2908c24e308777924a9c";
+
+  // const endpoint = "https://343d000d-f560-48cd-ad81-3e6197ab4a41-us-east-2.apps.astra.datastax.com";
+  // const token = "AstraCS:iumnAmyrXRffWPpgUotrTZia:5983fefa5056bb8b0347e9acd70230608af281a81026da70261b83ae01fa564e";
   // const endpoint = "https://86aa9693-ff4b-42d1-8a3d-a3e6d65b7d80-us-east-2.apps.astra.datastax.com";
   // const token = "AstraCS:wgxhHEEYccerYdqKsaTyQKox:4d0ac01c55062c11fc1e9478acedc77c525c0b278ebbd7220e1d873abd913119";
 //   const endpoint =
