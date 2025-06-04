324655ee4f4158bdb3951b2a83d99996b3f39d08
manfmexistence <ajju40959@gmail.com>
2025-06-04 13:26:06 +0600

    feat: enhance locale switcher with dropdown menu

--- www/app/[lang]/components/locale-switcher.tsx
+++ www/app/[lang]/components/locale-switcher.tsx
@@ -2,7 +2,15 @@
 
 import { usePathname } from "next/navigation";
 import Link from "next/link";
+import { Globe } from "lucide-react";
 import { i18n, type Locale } from "../../../i18n-config";
+import { Button } from "../../../components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "../../../components/ui/dropdown-menu";
 
 export default function LocaleSwitcher() {
   const pathname = usePathname();
@@ -13,18 +21,35 @@ export default function LocaleSwitcher() {
     return segments.join("/");
   };
 
+  const getCurrentLocale = () => {
+    if (!pathname) return i18n.defaultLocale;
+    const segments = pathname.split("/");
+    const localeFromPath = segments[1];
+    return i18n.locales.includes(localeFromPath as Locale) 
+      ? localeFromPath 
+      : i18n.defaultLocale;
+  };
+
   return (
-    <div>
-      <p>Locale switcher:</p>
-      <ul>
-        {i18n.locales.map((locale) => {
-          return (
-            <li key={locale}>
-              <Link href={redirectedPathname(locale)}>{locale}</Link>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="sm" className="gap-2">
+          <Globe className="h-4 w-4" />
+          {getCurrentLocale().toUpperCase()}
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="min-w-[120px]">
+        {i18n.locales.map((locale) => (
+          <DropdownMenuItem key={locale} asChild>
+            <Link 
+              href={redirectedPathname(locale)}
+              className="cursor-pointer"
+            >
+              {locale.toUpperCase()}
+            </Link>
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
